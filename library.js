077b330f7ea440fdff8d4e2f45370751a861c1ba
alagozfurkan <alagozf30@gmail.com>
2025-01-02 20:00:09 +0300

    redesign algorithm for displaying given objects in the library

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 let realDiv = document.querySelector(".bookcard");
 let containerDiv = document.querySelector(".container")
+
+
 function Book(title, author, pages, readstate) {
     this.title = title;
     this.author = author;
@@ -7,12 +9,32 @@ function Book(title, author, pages, readstate) {
     this.readstate = readstate;
 }
 
-let book1 = new Book("Sefiller", "Dostoyevski", 345, true);
 
-function addBookToLibrary() {
-    clonedDiv = realDiv.cloneNode(true);
-    clonedDiv.class = ".bookcard";
-    containerDiv.appendChild(clonedDiv);
-}
+let book1 = new Book("ahmet", "kara", 35, true);
+let book2 = new Book("mehmet", "Dostoyevski", 345, false);
 
-addBookToLibrary();
+
+const myLibrary = [book1, book2];
+
+
+
+function addBookToLibrary(array) {
+    for (let i = 0; i < array.length; i++) {
+        clonedDiv = realDiv.cloneNode(true);
+        clonedDiv.class = ".bookcard";
+        clonedDiv.querySelector("#bookname").textContent = array[i].title;
+        clonedDiv.querySelector("#author").textContent = array[i].author;
+        clonedDiv.querySelector("#pages").textContent = array[i].pages;
+        if (array[i].readstate === true) {
+            clonedDiv.querySelector("#readstate").textContent = "Okundu"
+        } else {
+            clonedDiv.querySelector("#readstate").textContent = "Okunacak"
+        }
+        containerDiv.appendChild(clonedDiv);
+      }
+    }
+    
+
+
+
+addBookToLibrary(myLibrary);
