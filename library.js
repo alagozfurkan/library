3d90719cb1573ef4b8527318a3c470a29e04d760
alagozfurkan <alagozf30@gmail.com>
2025-01-04 14:21:51 +0300

    implement a delete button

--- library.js
+++ library.js
@@ -9,10 +9,14 @@ let booktitle = document.querySelector("#title")
 var nodes = document.querySelector(".container").childNodes;
 
 
+
+
+var deleteButton = document.createElement("BUTTON");
+deleteButton.classList.add("delete");
 showDialog.addEventListener("click", function() {
-    while (nodes.length > 0) {
+    /*while (nodes.length > 0) {
         nodes[0].remove();
-    }
+    }*/
     dialog.showModal();
 });
 
@@ -20,10 +24,24 @@ closebutton.addEventListener("click", function() {
     dialog.close();
 })
 
+
+
+
+
+
+
+
+
+
+const myLibrary = [];
+
 confirmbutton.addEventListener("click", function(event) {
     let titleValue = document.getElementById("title").value
     event.preventDefault()
-    myLibrary.push(new Book(titleValue, "selam", 31, false));
+    let book = new Book(titleValue, "selam", 31, false);
+    console.log(book)
+    myLibrary.push(book);
+    console.log()
     addBookToLibrary(myLibrary)
     dialog.close()
 })
@@ -50,30 +68,54 @@ function Book(title, author, pages, readstate) {
     this.author = author;
     this.pages = pages;
     this.readstate = readstate;
+    this.place = myLibrary.length;
+    
 }
 
 
 
 
 
-const myLibrary = [];
+
+
+
 
 
 
 function addBookToLibrary(array) {
-    for (let i = 0; i < array.length; i++) {
+    
         clonedDiv = realDiv.cloneNode(true);
         clonedDiv.class = ".bookcard";
-        clonedDiv.querySelector("#bookname").textContent = array[i].title;
-        clonedDiv.querySelector("#author").textContent = array[i].author;
-        clonedDiv.querySelector("#pages").textContent = array[i].pages;
-        if (array[i].readstate === true) {
+        clonedDiv.querySelector("#bookname").textContent = array[array.length -1].title;
+        clonedDiv.querySelector("#author").textContent = array[array.length -1].author;
+        clonedDiv.querySelector("#pages").textContent = array[array.length -1].pages;
+        if (array[array.length -1].readstate === true) {
             clonedDiv.querySelector("#readstate").textContent = "Okundu"
         } else {
             clonedDiv.querySelector("#readstate").textContent = "Okunacak"
         }
+
+        var deleteButton = document.createElement("BUTTON");
+
+
+
+        deleteButton.addEventListener("click", function() {
+           deleteButton.parentElement.remove()
+            
+        })
+
+
+
+
+
+
+
+
+        deleteButton.classList.add("delete");
+        deleteButton.textContent = "Delete";
         containerDiv.appendChild(clonedDiv);
-      }
+        clonedDiv.appendChild(deleteButton);
+      
     }
     
 
